2e59caf66a11cd3d2bb8639ad4356f0f47a0b94b
nickfanny1222 <132698504+nickfanny1222@users.noreply.github.com>
2023-11-28 22:39:08 -0300

    configuracao vercel

--- project/core/settings.py
+++ project/core/settings.py
@@ -134,9 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+#]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
