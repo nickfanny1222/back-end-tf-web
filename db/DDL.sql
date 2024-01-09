5a5cf1ef30fa892c3a76cd612fc55f6de2683934
nickfanny1222 <132698504+nickfanny1222@users.noreply.github.com>
2024-01-09 19:00:05 +0000

    Modificado - Banco de dados

--- db/DDL.sql
+++ db/DDL.sql
@@ -12,14 +12,25 @@ CREATE TABLE usuario (
     id SERIAL NOT NULL,
     nome VARCHAR(50) NOT NULL,
     nome_completo VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     telefone VARCHAR(30) NOT NULL,
     curso INT NOT NULL,
     estado INT NOT NULL,
     ativo BOOLEAN NOT NULL,
+    profile_image VARCHAR(255) DEFAULT NULL,
     PRIMARY KEY (id)
 );
+
+/* TABELA contato */
+
+CREATE TABLE contato (
+    id SERIAL NOT NULL,
+    usuario INT NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    link VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (usuario) REFERENCES usuario(id) ON UPDATE CASCADE ON DELETE CASCADE
+)
 
 /* TABELA habilidade */
 
